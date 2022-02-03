7325902d94b6dfc2ab6f7dcbe617392b8eeebf48
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-03 19:55:26 +0200

    Created the navbar and updated the readme file

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,29 @@
-const Navbar = () => {
-	return (
-		<div>
-			<h1>This is the Navbar</h1>
-		</div>
-	);
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import Link from 'next/link';
+
+const NavBar = () => {
+    return (
+        <>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Link href="#nav" passHref>
+                        <Navbar.Brand>Home</Navbar.Brand>
+                    </Link>
+                    <Nav className="me-auto">
+                        <Link href="#dashboard" passHref>
+                            <Nav.Link>Dashboard</Nav.Link>
+                        </Link>
+                        <Link href="#home" passHref>
+                            <Nav.Link>Dashboard</Nav.Link>
+                        </Link>
+                        <Button className="justify-content-right" variant="outline-success">
+                            Sign In
+                        </Button>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 };
 
-export default Navbar;
+export default NavBar;
