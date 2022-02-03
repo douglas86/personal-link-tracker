ac4e122e878e32e32aabf140bbcbb6dbab42840b
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-03 18:34:14 +0200

    Imported Layout component into the _app.js file

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar.jsx";
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Navbar />
+		</>
+	);
+};
+
+export default Layout;
---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+	return (
+		<div>
+			<h1>This is the Navbar</h1>
+		</div>
+	);
+};
+
+export default Navbar;
