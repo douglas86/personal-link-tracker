0e8a03b31e90087ec53177683a950a0eb487c482
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-07 10:33:49 +0200

    Created an Admin and Subscriber dash page

---
+++ components/Dashboard/Admin.jsx
@@ -0,0 +1,9 @@
+const Admin = () => {
+    return (
+        <div>
+            <h1>This is the admin page</h1>
+        </div>
+    );
+};
+
+export default Admin;
---
+++ pages/dashboard.jsx
@@ -0,0 +1,3 @@
+import { getSession, useSession } from 'next-auth/react';
+import prisma from '../lib/prisma';
+
