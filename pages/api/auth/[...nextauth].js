52a7f91e1924f888e9b265ebd3e1794e343cca84
douglas86 <douglasmaxton@gmail.com>
2022-02-10 20:40:42 +0200

    Implemeted Google login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,17 @@
 import NextAuth from 'next-auth/next';
+
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.NEXTAUTH_GITHUB_ID,
             clientSecret: process.env.NEXTAUTH_GITHUB_SECRET,
+        }),
+        GoogleProvider({
+            clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID,
+            clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET,
         }),
     ],
     callbacks: {
