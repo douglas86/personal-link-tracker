a0367647bd19a847d8d1c1e660723f3a87a783da
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-14 15:26:35 +0200

    Added email auth to next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,20 @@
 import NextAuth from 'next-auth/next';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
 
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import EmailProvider from 'next-auth/providers/email';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [
+        EmailProvider({
+            server: process.env.NEXTAUTH_EMAIL_SERVER,
+            from: process.env.NEXTAUTH_EMAIL_FROM,
+        }),
         GoogleProvider({
             clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID,
             clientSecret: process.env.NEXTAUTH_GOOGLE_CLIENT_SECRET,
