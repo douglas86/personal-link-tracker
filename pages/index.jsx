0f38b3f651bf0978c77d652d12678862e88fabc9
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-22 09:54:43 +0200

    Displays content from s3Bucket on home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,39 +1,48 @@
-import useSWR from 'swr';
-import { CardGroup, Card, Button } from 'react-bootstrap';
-import Link from 'next/link';
+import useSWR from "swr";
+import Link from "next/link";
 
 const Home = () => {
-    const fetcher = (url) => fetch(url).then((res) => res.json());
-    const { data } = useSWR('/api/AWS/s3', fetcher);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data } = useSWR("/api/AWS/s3", fetcher);
 
-    console.log('data', data);
+  console.log("data", data);
 
-    const listCategories =
-        data !== undefined
-            ? Object.entries(data.data).map(([k, v]) => (
-                  <Link href="/" key={k} passHref>
-                      <Card style={{ width: '10rem' }}>
-                          <Card.Img
-                              variant="top"
-                              src={`data:image/jpeg;base64,${v.Body}`}
-                          />
-                          <Card.Body>
-                              <Card.Title>Card Title</Card.Title>
-                              <Card.Text>{v.desc}</Card.Text>
-                          </Card.Body>
-                      </Card>
-                  </Link>
-              ))
-            : null;
+  const listCategories =
+    data !== undefined
+      ? Object.entries(data.data).map(([k, v]) => (
+          <Link href="/" key={k} passHref>
+            <a
+              className="bg-light p-3 col-md-4"
+              href=""
+              style={{ border: "1px solid red", margin: "1px 25%" }}
+            >
+              <div>
+                <div className="row">
+                  <div className="col-md-4">
+                    <img
+                      className="pr-3"
+                      src={`data:image/jpeg;base64,${v.Body}`}
+                      style={{ width: "80px", height: "auto" }}
+                      alt={v.name}
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <h3>{v.name}</h3>
+                    <p>{v.description}</p>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        ))
+      : null;
 
-    return (
-        <div>
-            <h1>This is the home page</h1>
-            <div>
-                <CardGroup>{listCategories}</CardGroup>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is the home page</h1>
+      <div className="row">{listCategories}</div>
+    </div>
+  );
 };
 
 export default Home;
