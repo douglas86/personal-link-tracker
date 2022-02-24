e8ff74f100c4a7510a95d8fe15c4695ffa5da1e2
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-24 12:26:28 +0200

    stream lined code 2 draw faster from s3 bucket

--- pages/api/AWS/s3.js
+++ pages/api/AWS/s3.js
@@ -1,72 +1,110 @@
 import { s3 } from "../../../lib/s3Client";
 import { CreateCategory, Params } from "./helpers/s3helpers";
-import prisma from "../../../lib/prisma";
+// import prisma from "../../../lib/prisma";
 
-const getObject = async (bucket, objectKey) => {
-  try {
-    const params = {
-      Bucket: bucket,
-      Key: objectKey,
-    };
-    const data = await s3.getObject(params).promise();
-    return data.Body.toString("base64");
-  } catch (e) {
-    throw new Error(`could not retrieve file from s3: ${e.message}`);
-  }
-};
+// const getObject = async (bucket, objectKey) => {
+//   try {
+//     const params = {
+//       Bucket: bucket,
+//       Key: objectKey,
+//     };
+//     const data = await s3.getObject(params).promise();
+//     return data.Body.toString("base64");
+//   } catch (e) {
+//     throw new Error(`could not retrieve file from s3: ${e.message}`);
+//   }
+// };
+
+let contents = [];
 
 export default async function handler(req, res) {
   const { method, body } = req;
 
+  const getObject = async (resources, params) => {
+    resources.Contents.map(async (item) => {
+      let goParams = {
+        Bucket: params.Bucket,
+        Key: item.Key,
+      };
+      await s3
+        .getObject(goParams)
+        .promise()
+        .then((r) => {
+          contents.push({ title: item.Key, image: r.Body.toString("base64") });
+          if (resources.Contents.length === contents.length) {
+            res.status(200).json({
+              contents,
+            });
+          }
+        });
+    });
+  };
+
   switch (method) {
     case "GET":
+      console.log("this");
       try {
-        const categories = await prisma.category.findMany();
-
-        async function getS3Data() {
-          let d;
-          const params = {
-            Bucket: process.env.NEXT_PUBLIC_S3BUCKET_NAME,
-          };
-
-          try {
-            d = await s3.listObjectsV2(params).promise();
-          } catch (e) {
-            throw e;
-          }
-          let content = [];
-          let count = 0;
-          for (let currentValue of d.Contents) {
-            console.log("u", currentValue);
-            if (currentValue.Size > 0) {
-              let goParams = {
-                Bucket: params.Bucket,
-                Key: currentValue.Key,
-              };
-              let data;
-              try {
-                data = await s3.getObject(goParams).promise();
-              } catch (e) {
-                throw e;
-              }
-              content.push({
-                id: categories[count].id,
-                name: categories[count].name,
-                description: categories[count].description,
-                Size: data.ContentLength,
-                Body: data.Body.toString("base64"),
-              });
-            }
-            count += 1;
-          }
-          return content;
-        }
-        res.status(200).json({
-          data: await getS3Data(),
-        });
+        const params = {
+          Bucket: process.env.NEXT_PUBLIC_S3BUCKET_NAME,
+        };
+        await s3
+          .listObjectsV2(params)
+          .promise()
+          .then(async (r) => {
+            let result = await getObject(r, params);
+            return result;
+          });
       } catch (err) {
-        console.log("error", err);
+        console.log("err", err);
       }
+      // try {
+
+      //   const categories = await prisma.category.findMany();
+      //
+      //   async function getS3Data() {
+      //     let d;
+      //     const params = {
+      //       Bucket: process.env.NEXT_PUBLIC_S3BUCKET_NAME,
+      //     };
+      //
+      //     try {
+      //       d = await s3.listObjectsV2(params).promise();
+      //     } catch (e) {
+      //       throw e;
+      //     }
+      //     let content = [];
+      //     let count = 0;
+      //     for (let currentValue of d.Contents) {
+      //       console.log("u", currentValue);
+      //       if (currentValue.Size > 0) {
+      //         let goParams = {
+      //           Bucket: params.Bucket,
+      //           Key: currentValue.Key,
+      //         };
+      //         let data;
+      //         try {
+      //           data = await s3.getObject(goParams).promise();
+      //         } catch (e) {
+      //           throw e;
+      //         }
+      //         content.push({
+      //           id: categories[count].id,
+      //           name: categories[count].name,
+      //           description: categories[count].description,
+      //           Size: data.ContentLength,
+      //           Body: data.Body.toString("base64"),
+      //         });
+      //       }
+      //       count += 1;
+      //     }
+      //     return content;
+      //   }
+      //   res.status(200).json({
+      //     data: await getS3Data(),
+      //   });
+      // } catch (err) {
+      //   console.log("error", err);
+      // }
       break;
     case "POST":
       try {
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import useSWR from "swr";
 import Link from "next/link";
+// import { s3 } from "../lib/s3Client";
+// import prisma from "../lib/prisma.js";
 
 const Home = () => {
   const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -9,38 +11,65 @@ const Home = () => {
 
   const listCategories =
     data !== undefined
-      ? Object.entries(data.data).map(([k, v]) => (
-          <Link href="/" key={k} passHref>
-            <a
-              className="bg-light p-3 col-md-4"
-              href=""
-              style={{ border: "1px solid red", margin: "1px 25%" }}
-            >
-              <div>
-                <div className="row">
-                  <div className="col-md-4">
-                    <img
-                      className="pr-3"
-                      src={`data:image/jpeg;base64,${v.Body}`}
-                      style={{ width: "80px", height: "auto" }}
-                      alt={v.name}
-                    />
-                  </div>
-                  <div className="col-md-8">
-                    <h3>{v.name}</h3>
-                    <p>{v.description}</p>
-                  </div>
-                </div>
+      ? Object.entries(data.contents).map(([k, v]) => (
+          <div
+            className="bg-light p-3 col-md-4"
+            style={{ border: "1px solid red", margin: "1px 25%" }}
+            key={k}
+          >
+            <div className="row">
+              <div className="col-md-4">
+                <img
+                  className="pr-3"
+                  src={`data:image/jpeg;base64,${v.image}`}
+                  style={{ width: "80px", height: "auto" }}
+                  alt={v.title}
+                />
               </div>
-            </a>
-          </Link>
+            </div>
+            <div className="col-md-8">
+              <h1>{v.title}</h1>
+            </div>
+          </div>
         ))
       : null;
+
+  // const listCategories =
+  //   data !== undefined
+  //     ? Object.entries(data.contents).map(([k, v], i) => (
+  //         <div key={k}>
+  //           <Link href={`/${i}`} key={k} passHref>
+  //             {console.log("Body", v)}
+  //             <a
+  //               className="bg-light p-3 col-md-4"
+  //               href=""
+  //               style={{ border: "1px solid red", margin: "1px 25%" }}
+  //             >
+  //               <div>
+  //                 <div className="row">
+  //                   <div className="col-md-4">
+  //                     <img
+  //                       className="pr-3"
+  //                       src={`data:image/jpeg;base64,${v.image}`}
+  //                       style={{ width: "80px", height: "auto" }}
+  //                       alt={v.title}
+  //                     />
+  //                   </div>
+  //                   <div className="col-md-8">
+  //                     <h3>{v.title}</h3>
+  //                   </div>
+  //                 </div>
+  //               </div>
+  //             </a>
+  //           </Link>
+  //         </div>
+  //       ))
+  //     : null;
 
   return (
     <div>
       <h1>This is the home page</h1>
-      <div className="row">{listCategories}</div>
+      <div>{listCategories}</div>
     </div>
   );
 };
