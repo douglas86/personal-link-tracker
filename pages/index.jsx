b815079c0d2e380033ae820c6eba39b5c57e1bc8
douglas86 <douglasmaxton@gmail.com>
2022-02-28 14:52:34 +0200

    Designed a flexbox for front page cards

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,5 @@
 import useSWR from "swr";
-import Link from "next/link";
-// import { s3 } from "../lib/s3Client";
-// import prisma from "../lib/prisma.js";
+import styles from "../public/styles/index.module.css";
 
 const Home = () => {
   const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -9,67 +7,27 @@ const Home = () => {
 
   console.log("data", data);
 
-  const listCategories =
-    data !== undefined
-      ? Object.entries(data.contents).map(([k, v]) => (
-          <div
-            className="bg-light p-3 col-md-4"
-            style={{ border: "1px solid red", margin: "1px 25%" }}
-            key={k}
-          >
-            <div className="row">
-              <div className="col-md-4">
-                <img
-                  className="pr-3"
-                  src={`data:image/jpeg;base64,${v.image}`}
-                  style={{ width: "80px", height: "auto" }}
-                  alt={v.title}
-                />
-              </div>
-            </div>
-            <div className="col-md-8">
-              <h1>{v.title}</h1>
-            </div>
-          </div>
-        ))
-      : null;
-
-  // const listCategories =
-  //   data !== undefined
-  //     ? Object.entries(data.contents).map(([k, v], i) => (
-  //         <div key={k}>
-  //           <Link href={`/${i}`} key={k} passHref>
-  //             {console.log("Body", v)}
-  //             <a
-  //               className="bg-light p-3 col-md-4"
-  //               href=""
-  //               style={{ border: "1px solid red", margin: "1px 25%" }}
-  //             >
-  //               <div>
-  //                 <div className="row">
-  //                   <div className="col-md-4">
-  //                     <img
-  //                       className="pr-3"
-  //                       src={`data:image/jpeg;base64,${v.image}`}
-  //                       style={{ width: "80px", height: "auto" }}
-  //                       alt={v.title}
-  //                     />
-  //                   </div>
-  //                   <div className="col-md-8">
-  //                     <h3>{v.title}</h3>
-  //                   </div>
-  //                 </div>
-  //               </div>
-  //             </a>
-  //           </Link>
-  //         </div>
-  //       ))
-  //     : null;
-
   return (
     <div>
-      <h1>This is the home page</h1>
-      <div>{listCategories}</div>
+      <h1 className={styles.title}>Different types of categories</h1>
+      <div className={styles.flex_container}>
+        {data !== undefined
+          ? Object.entries(data.contents).map(([k, v]) => (
+              <div key={k} className={styles.contents}>
+                <div className={styles.flex_image}>
+                  <img
+                    className={styles.image}
+                    src={`data:image/jpeg;base64,${v.image}`}
+                    alt={v.title}
+                  />
+                </div>
+                <div className={styles.title}>
+                  <h5>{v.title}</h5>
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 };
