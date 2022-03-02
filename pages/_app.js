d95d490d79693e9f2e1e5d25913509b201c454bc
douglas86 <douglasmaxton@gmail.com>
2022-03-02 09:22:31 +0200

    Created a context file for the home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
-import Layout from '../components/Layout.jsx';
-import '../styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { SessionProvider } from 'next-auth/react';
+import Layout from "../components/Layout.jsx";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-    return (
-        <>
-            <SessionProvider session={session}>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </SessionProvider>
-        </>
-    );
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
+  );
 }
 
 export default MyApp;
