e6e5cc01c5378176c30a9150e64d7e2f50c0a9c1
douglas86 <douglasmaxton@gmail.com>
2022-03-15 09:21:50 +0200

    Created the homepage layout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,90 +10,104 @@ import Image from 'next/image';
 import { Get } from './api';
 
 // Images
-import Home from '../public/Images/home.jpeg';
+import Home from '../public/static/Images/home.jpeg';
 
 const NavBar = () => {
-  // this shows and hides popup for mouse hover on home image
-  const [isShownHome, setIsShownHome] = useState(false);
-  const [isShownDashboard, setIsShownDashboard] = useState(false);
+    // this shows and hides popup for mouse hover on home image
+    const [isShownHome, setIsShownHome] = useState(false);
+    const [isShownDashboard, setIsShownDashboard] = useState(false);
 
-  const { data: session } = useSession();
-  const router = useRouter();
+    const { data: session } = useSession();
+    const router = useRouter();
 
-  // as soon as current page url changed it sets isShown to false
-  useEffect(() => {
-    setIsShownHome(false);
-    setIsShownDashboard(false);
-  }, [router.pathname]);
+    // as soon as current page url changed it sets isShown to false
+    useEffect(() => {
+        setIsShownHome(false);
+        setIsShownDashboard(false);
+    }, [router.pathname]);
 
-  return (
-    <>
-      <Navbar bg="light" variant="light">
-        <Container>
-          {router.pathname === '/' ? (
-            <Link href="/" passHref>
-              <a onClick={() => router.reload(window.location.pathname)}>
-                <Image src={Home} alt="home page" width={30} height={30} />
-              </a>
-            </Link>
-          ) : null}
-          {router.pathname !== '/' ? (
-            <>
-              <Link href="/" passHref>
-                <a
-                  onMouseEnter={() => setIsShownHome(true)}
-                  onMouseLeave={() => setIsShownHome(false)}
-                >
-                  <Image src={Home} alt="home page" width={30} height={30} />
-                </a>
-              </Link>
+    return (
+        <>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    {router.pathname === '/' ? (
+                        <Link href="/" passHref>
+                            <a
+                                onClick={() =>
+                                    router.reload(window.location.pathname)
+                                }
+                            >
+                                <Image
+                                    src={Home}
+                                    alt="home page"
+                                    width={30}
+                                    height={30}
+                                />
+                            </a>
+                        </Link>
+                    ) : null}
+                    {router.pathname !== '/' ? (
+                        <>
+                            <Link href="/" passHref>
+                                <a
+                                    onMouseEnter={() => setIsShownHome(true)}
+                                    onMouseLeave={() => setIsShownHome(false)}
+                                >
+                                    <Image
+                                        src={Home}
+                                        alt="home page"
+                                        width={30}
+                                        height={30}
+                                    />
+                                </a>
+                            </Link>
 
-              {isShownHome && <h4>Home Page</h4>}
-            </>
-          ) : null}
-          <Nav className="ml-auto">
-            {session !== undefined &&
-            session !== null &&
-            router.pathname !== '/dashboard' ? (
-              <>
-                <Link href="/dashboard" passHref>
-                  <Nav.Link>Dashboard</Nav.Link>
-                </Link>
-              </>
-            ) : null}
-            {router.pathname !== '/user/link/create' ? (
-              <>
-                <Link href="/user/link/create" passHref>
-                  <Nav.Link>Submit a Link</Nav.Link>
-                </Link>
-              </>
-            ) : null}
-          </Nav>
-          {session ? (
-            <Button
-              className="justify-content-right"
-              variant="outline-success"
-              onClick={() =>
-                signOut({
-                  callbackUrl: `${window.location.origin}/dashboard`,
-                })
-              }
-            >
-              Sign Out
-            </Button>
-          ) : (
-            <Button
-              className="justify-content-right"
-              variant="outline-primary"
-              onClick={() => signIn()}
-            >
-              Sign In
-            </Button>
-          )}
-        </Container>
-      </Navbar>
-    </>
-  );
+                            {isShownHome && <h4>Home Page</h4>}
+                        </>
+                    ) : null}
+                    <Nav className="ml-auto">
+                        {session !== undefined &&
+                        session !== null &&
+                        router.pathname !== '/dashboard' ? (
+                            <>
+                                <Link href="/dashboard" passHref>
+                                    <Nav.Link>Dashboard</Nav.Link>
+                                </Link>
+                            </>
+                        ) : null}
+                        {router.pathname !== '/user/link/create' ? (
+                            <>
+                                <Link href="/user/link/create" passHref>
+                                    <Nav.Link>Submit a Link</Nav.Link>
+                                </Link>
+                            </>
+                        ) : null}
+                    </Nav>
+                    {session ? (
+                        <Button
+                            className="justify-content-right"
+                            variant="outline-success"
+                            onClick={() =>
+                                signOut({
+                                    callbackUrl: `${window.location.origin}/dashboard`,
+                                })
+                            }
+                        >
+                            Sign Out
+                        </Button>
+                    ) : (
+                        <Button
+                            className="justify-content-right"
+                            variant="outline-primary"
+                            onClick={() => signIn()}
+                        >
+                            Sign In
+                        </Button>
+                    )}
+                </Container>
+            </Navbar>
+        </>
+    );
 };
 
 export default NavBar;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,40 @@
+import Image from 'next/image';
+
 import { Fetcher } from '../API';
+import styles from '../public/static/styles/index.module.css';
 
 const Home = () => {
-  const fetching = Fetcher('/api/AWS/s3');
-  console.log('fetching', fetching);
-  return <h1>This is the home page</h1>;
+    const fetching = Fetcher('/api/AWS/s3');
+    console.log('fetching', fetching);
+    return (
+        <div>
+            <h1 className={styles.title}>Browse Tutorial/Courses</h1>
+            <div className={styles.flex_container}>
+                {fetching !== undefined
+                    ? Object.entries(fetching.contents).map(([k, v]) => (
+                          <button key={k} className={styles.button}>
+                              <div className={styles.contents}>
+                                  <div className={styles.flex_image}>
+                                      <Image
+                                          className={styles.image}
+                                          src={`data:image/jpeg;base64,${v.image}`}
+                                          alt={v.title}
+                                          width={400}
+                                          height={200}
+                                      />
+                                  </div>
+                                  <div className={styles.title}>
+                                      <h5>
+                                          {v.title.split('.')[0].split('/')[1]}
+                                      </h5>
+                                  </div>
+                              </div>
+                          </button>
+                      ))
+                    : null}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
