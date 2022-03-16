fc6a3a90574e53b04b28f5f31327f12e9533b494
douglas86 <douglasmaxton@gmail.com>
2022-03-16 23:49:24 +0200

    Items on admin dash align next to one another

--- Dashboard/Admin/Read.jsx
+++ Dashboard/Admin/Read.jsx
@@ -15,33 +15,37 @@ const Read = () => {
   return (
     <>
       <Header />
-      {fetching !== undefined
-        ? Object.entries(fetching.data).map(([k, v]) => (
-            <div key={k} className={styles.contents}>
-              <div className={styles.flex_image}>
-                <Image
-                  className={styles.image}
-                  src={`data:image/jpeg;base64,${v.image}`}
-                  alt={v.title}
-                  width={400}
-                  height={270}
-                />
+      <div className={styles.flex_container}>
+        {fetching !== undefined
+          ? Object.entries(fetching.data).map(([k, v]) => (
+              <div key={k} className={styles.contents}>
+                <div className={styles.flex_image}>
+                  <Image
+                    className={styles.image}
+                    src={`data:image/jpeg;base64,${v.image}`}
+                    alt={v.title}
+                    width={400}
+                    height={270}
+                  />
+                </div>
+                <div className={styles.title}>
+                  <h5>{v.title.split('.')[0].split('/')[1]}</h5>
+                  <button
+                    className={`btn btn-outline-success ${styles.button}`}
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => handleConfirm(v)}
+                    className={`btn btn-outline-danger ${styles.button}`}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <div className={styles.title}>
-                <h5>{v.title.split('.')[0].split('/')[1]}</h5>
-                <button className={`btn btn-outline-success ${styles.button}`}>
-                  Update
-                </button>
-                <button
-                  onClick={() => handleConfirm(v)}
-                  className={`btn btn-outline-danger ${styles.button}`}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          ))
-        : null}
+            ))
+          : null}
+      </div>
     </>
   );
 };
