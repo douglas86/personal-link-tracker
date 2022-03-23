266ed99b0e59b3212cb7fa0881170a6947e7da84
douglas86 <douglasmaxton@gmail.com>
2022-03-23 19:41:07 +0200

    Gather data from getServerSideProps to browser

--- pages/links/[slug].jsx
+++ pages/links/[slug].jsx
@@ -1,9 +1,47 @@
-const Links = () => {
+import prisma from '../../lib/prisma';
+import { s3 } from '../../lib/s3Client';
+import { keys } from '../../lib/keys';
+
+const Links = (props) => {
+  const prop = JSON.parse(props.data);
+  console.log('prop', prop);
   return (
     <div>
       <h1>This is links</h1>
     </div>
   );
+};
+
+export const getServerSideProps = async ({ query }) => {
+  const { slug } = query;
+  const Prisma = await prisma.category.findFirst({
+    where: {
+      title: slug,
+    },
+  });
+  const { title, description, s3BucketKey } = Prisma;
+
+  const params = {
+    Bucket: keys.aws.s3Bucket,
+    Key: s3BucketKey,
+  };
+
+  const S3 = await s3
+    .getObject(params)
+    .promise()
+    .then((res) => {
+      return res;
+    });
+
+  const data = {
+    title,
+    description,
+    image: S3.Body.toString('base64'),
+  };
+
+  return {
+    props: { data: JSON.stringify(data) },
+  };
 };
 
 export default Links;
