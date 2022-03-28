12585f7f1c67a14797bf7cf2bc5fc25d194ece0c
douglas86 <douglasmaxton@gmail.com>
2022-03-28 15:06:44 +0200

    Deleted all unwanted comments and console.logs

--- pages/user/link/create.jsx
+++ pages/user/link/create.jsx
@@ -9,16 +9,7 @@ const create = (props) => {
   const categories = JSON.parse(props.result);
   const { state, setState, showCategories, showTypes, showMedium, showForm } =
     createHelpers(categories);
-  const {
-    category,
-    formTitle,
-    formURL,
-    type,
-    medium,
-    message,
-    showAlert,
-    alertColor,
-  } = state;
+  const { message, showAlert, alertColor } = state;
 
   const [screenSize, setScreenSize] = useState({
     dynamicWidth: 0,
@@ -44,8 +35,6 @@ const create = (props) => {
       }, 10000);
     }
   }, [showAlert]);
-
-  console.log('state', state);
 
   return (
     <Container>
@@ -100,40 +89,3 @@ export const getServerSideProps = async () => {
 };
 
 export default create;
-
-// import { CreateProvider } from '../../../components/pages/PageContext/user/link/CreateContext';
-// import Create from '../../../components/pages/user/link/Create';
-// import { getSession } from 'next-auth/react';
-//
-// const CreatePage = (props) => {
-//   const response = props ? JSON.parse(props.data) : undefined;
-//   const user = props ? JSON.parse(props.user).id : undefined;
-//   return (
-//     <div>
-//       <CreateProvider>
-//         <Create response={response} user={user} />
-//       </CreateProvider>
-//     </div>
-//   );
-// };
-//
-// export const getServerSideProps = async (ctx) => {
-//   const session = await getSession(ctx);
-//   const user = session
-//     ? await prisma.user.findUnique({
-//         where: {
-//           email: session.user.email,
-//         },
-//       })
-//     : null;
-//   const result = await prisma.category.findMany();
-//   const data = JSON.stringify(result);
-//   return {
-//     props: {
-//       data,
-//       user: JSON.stringify(user),
-//     },
-//   };
-// };
-//
-// export default CreatePage;
