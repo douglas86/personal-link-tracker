de56e4becd8f06a8ffde2e106f9ac24a15d2dbdd
douglas86 <douglasmaxton@gmail.com>
2022-04-18 14:58:44 +0200

    Setup blueprint for user Dashboard

--- Dashboard/Subscriber/index.jsx
+++ Dashboard/Subscriber/index.jsx
@@ -1,5 +1,36 @@
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { Container } from 'react-bootstrap';
+
+import styles from './styles/index.module.css';
+
 const Subscriber = () => {
-    return <h1>This is the subscriber page</h1>;
+    const { data: session } = useSession();
+
+    return (
+        <div>
+            <Container>
+                <h1 className={styles.heading}>
+                    This is {session.user.name}'s Dashboard
+                </h1>
+                <hr />
+                <div className={styles.flexbox}>
+                    <div className={styles.leftSide}>
+                        <Link href="/user/link/create" passHref>
+                            <a>Submit a link</a>
+                        </Link>
+                        <br />
+                        <Link href="/user/profile/update" passHref>
+                            <a>Update a profile</a>
+                        </Link>
+                    </div>
+                    <div className={styles.rightSide}>
+                        <h1>This is the right</h1>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
 };
 
 export default Subscriber;
