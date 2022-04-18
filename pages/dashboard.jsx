7e38a759821429307418205a99fb24dd5a857a4f
douglas86 <douglasmaxton@gmail.com>
2022-04-18 13:09:54 +0200

    Rectified loging in with dashboard

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -10,43 +10,47 @@ import { AdminProvider } from '../Context/Dashboard/Admin/AdminContext';
 import { SubscriberProvider } from '../Context/Dashboard/Subscriber/SubscriberContext';
 
 const Dashboard = ({ data }) => {
-  const { session } = useSession();
+    const { data: session } = useSession();
 
-  const Role = (role) => {
-    switch (role) {
-      case 'admin':
-        return (
-          <AdminProvider>
-            <Admin />
-          </AdminProvider>
-        );
-      default:
-        return (
-          <SubscriberProvider>
-            <Subscriber />
-          </SubscriberProvider>
-        );
-    }
-  };
+    const Role = (role) => {
+        switch (role) {
+            case 'admin':
+                return (
+                    <AdminProvider>
+                        <Admin />
+                    </AdminProvider>
+                );
+            default:
+                return (
+                    <SubscriberProvider>
+                        <Subscriber />
+                    </SubscriberProvider>
+                );
+        }
+    };
 
-  return (
-    <>
-      {session ? Role(JSON.parse(data).role) : <h1>You are not signed in</h1>}
-    </>
-  );
+    return (
+        <>
+            {session ? (
+                Role(JSON.parse(data).role)
+            ) : (
+                <h1>You are not signed in</h1>
+            )}
+        </>
+    );
 };
 
 export const getServerSideProps = async (ctx) => {
-  const session = await getSession(ctx);
-  const user = session
-    ? await prisma.user.findUnique({
-        where: {
-          email: session.user.email,
-        },
-      })
-    : null;
-  const data = JSON.stringify(user);
-  return { props: { data } };
+    const session = await getSession(ctx);
+    const user = session
+        ? await prisma.user.findUnique({
+              where: {
+                  email: session.user.email,
+              },
+          })
+        : null;
+    const data = JSON.stringify(user);
+    return { props: { data } };
 };
 
 export default Dashboard;
