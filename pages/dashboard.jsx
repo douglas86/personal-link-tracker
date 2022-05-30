0b3a419184d0d6639a04f17994990202ee0ef009
douglas86 <douglasmaxton@gmail.com>
2022-05-30 16:26:35 +0200

    Deleted getServerSideProps from dashboard

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,51 +1,48 @@
-import { getSession, useSession } from 'next-auth/react';
-
-import prisma from '../lib/prisma';
+import { useSession } from 'next-auth/react';
 
 import Admin from '../Dashboard/Admin';
 import Subscriber from '../Dashboard/Subscriber';
 
+import { spinner } from '../components/atom/spinner';
 import { AdminProvider } from '../Context/Dashboard/Admin/AdminContext';
 import { SubscriberProvider } from '../Context/Dashboard/Subscriber/SubscriberContext';
+import { GetRoute } from '../API/index2';
 
-const Dashboard = ({ data }) => {
-  const { data: session } = useSession();
+const Dashboard = () => {
+    const { data: session } = useSession();
 
-  const Role = (role) => {
-    switch (role) {
-      case 'admin':
-        return (
-          <AdminProvider>
-            <Admin />
-          </AdminProvider>
-        );
-      default:
-        return (
-          <SubscriberProvider>
-            <Subscriber />
-          </SubscriberProvider>
-        );
-    }
-  };
+    const fetcher = GetRoute(`/api/user`).data;
 
-  return (
-    <>
-      {session ? Role(JSON.parse(data).role) : <h1>You are not signed in</h1>}
-    </>
-  );
-};
+    const Role = (role) => {
+        switch (role) {
+            case 'admin':
+                return (
+                    <AdminProvider>
+                        <Admin />
+                    </AdminProvider>
+                );
+            default:
+                return (
+                    <SubscriberProvider>
+                        <Subscriber />
+                    </SubscriberProvider>
+                );
+        }
+    };
 
-export const getServerSideProps = async (ctx) => {
-  const session = await getSession(ctx);
-  const user = session
-    ? await prisma.user.findUnique({
-        where: {
-          email: session.user.email,
-        },
-      })
-    : null;
-  const data = JSON.stringify(user);
-  return { props: { data } };
+    return (
+        <>
+            {session ? (
+                fetcher === undefined ? (
+                    spinner()
+                ) : (
+                    Role(fetcher[0].role)
+                )
+            ) : (
+                <h1>You are not signed in</h1>
+            )}
+        </>
+    );
 };
 
 export default Dashboard;
