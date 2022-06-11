f295a3d2064b516af7a89da4276b4e6f2fa8e940
douglas86 <douglasmaxton@gmail.com>
2022-06-11 16:25:56 +0200

    Created a form using react hook form with react quill

--- components/organism/AdminTabs/CreateCategory.jsx
+++ components/organism/AdminTabs/CreateCategory.jsx
@@ -1,19 +1,11 @@
-import { useContext } from 'react';
-
-import { AdminContext } from '../../../Context/AdminContext';
-
-import Form from '../Form';
+import Form from "../Form";
 
 const CreateCategory = () => {
-    const { isTab, setIsTab } = useContext(AdminContext);
-
-    console.log('isTab', isTab);
-
-    return (
-        <>
-            <Form />
-        </>
-    );
+  return (
+    <>
+      <Form />
+    </>
+  );
 };
 
 export default CreateCategory;
