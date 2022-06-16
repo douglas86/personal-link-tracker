4f2cebbba39a0e762e437df1fc76865fbad80e2a
douglas86 <douglasmaxton@gmail.com>
2022-06-16 14:03:41 +0200

    Deleted all unwanted files in molecule
    Signed-off-by: douglas86 <douglasmaxton@gmail.com>

--- Dashboard/Admin/UpdateCategoriesTab/index.jsx
+++ Dashboard/Admin/UpdateCategoriesTab/index.jsx
@@ -1,10 +1,8 @@
-import { useContext } from 'react';
-import { Container } from 'react-bootstrap';
+import { useContext } from "react";
+import { Container } from "react-bootstrap";
 
-import { AdminContext } from '../../../Context/Dashboard/Admin/AdminContext.jsx';
-import Form from '../../../components/Admin/form.jsx';
-import styles from '../styles/Update.module.css';
-import Header from '../../../components/Admin/header.jsx';
+import { AdminContext } from "../../../Context/Dashboard/Admin/AdminContext.jsx";
+import styles from "../styles/Update.module.css";
 
 const UpdateCategory = () => {
   const context = useContext(AdminContext);
@@ -12,15 +10,11 @@ const UpdateCategory = () => {
   return (
     <div>
       <Container>
-        <Header />
         <div className={styles.flex_container}>
-          <div className={styles.flex_form}>
-            <Form />
-          </div>
           <div className={styles.flex_img}>
             <img
               src={
-                context.state.image.indexOf('data') !== -1
+                context.state.image.indexOf("data") !== -1
                   ? `${context.state.image}`
                   : `data:image/jpeg;base64,${context.state.image}`
               }
